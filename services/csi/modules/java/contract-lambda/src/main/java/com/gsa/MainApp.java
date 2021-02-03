499ddca8562ada0db1ef3dd317f4f26603125e05
Balu Vyamajala <balu.vyamajala@initechglobal.com>
2021-02-03 18:17:47 -0500

    dummy lambda for testing.

---
+++ services/csi/modules/java/contract-lambda/src/main/java/com/gsa/MainApp.java
@@ -0,0 +1,23 @@
+package com.gsa;
+
+import java.util.function.Function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
+
+	@Bean
+	public Function<String, String> getContract() {
+		return contractId -> {
+			return "Sucess";
+		};
+	}
+
+}
