d2dffc776f9ce65b437eb90551eec4832ab259b1
Balu Vyamajala <balu.vyamajala@initechglobal.com>
2021-02-17 09:54:46 -0500

    nsn api based project.

---
+++ services/nsn/infrastructure/cdk/src/nsn-api-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from '@aws-cdk/core';
+import { EnvHelper } from './helper/env-helper';
+import { CrossStackImporter } from './helper/CrossStackImporter';
+import { EnvParameters } from './models/env-parms';
+
+export class NsnApiStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const stackContext = this.node.tryGetContext(`${id}-${process.env.SHORT_ENV}`);
+        const envParameters: EnvParameters = new EnvHelper().getEnvironmentParams(stackContext);
+        console.log('envParameters', envParameters);
+
+        const crossStackImporter = new CrossStackImporter(this, 'corss-stack-imports', envParameters);
+    }
+}
