549cf313f3207c67ce98fa17f77cc753f52be2ed
Balu Vyamajala <balu.vyamajala@initechglobal.com>
2021-02-22 11:27:02 -0500

    xray and api gateway permissions

--- services/shared/infrastructure/cdk-iam/src/helper/builder/builder-factory.ts
+++ services/shared/infrastructure/cdk-iam/src/helper/builder/builder-factory.ts
@@ -1,11 +1,17 @@
+import { ApiGatewayBuilder } from './apigateway-builder';
 import { CloudwatchLogsBuilder } from './cloudwatch-logs-builder';
 import { BaseBuilder } from './common/BaseBulider';
 import { DynamoBuilder } from './dynamo-builder';
+import { XrayBuilder } from './xray-builder';
 
 function getPolicyBuilder(permission: string): BaseBuilder {
     if (permission.startsWith('dynamo')) return new DynamoBuilder(permission);
 
     if (permission.startsWith('logs')) return new CloudwatchLogsBuilder(permission);
+
+    if (permission.startsWith('apigateway')) return new ApiGatewayBuilder(permission);
+
+    if (permission.startsWith('xray')) return new XrayBuilder(permission);
 
     throw Error(`Policy Builder entry missing in builder-factory.js for ${permission}`);
 }
