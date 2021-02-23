bed5be241d52bcfd723a8d809a9c3a9bcdb9abcf
ruchir <ruchir.mehta@gsa.gov>
2021-02-22 19:18:08 -0500

    [PI-1][S-1] - adding Logger for ServiceHandler ... GetContractData

--- services/cis/modules/java/contractinformationservice/src/main/java/gov/gsa/fas/contractservice/handler/ServiceHandler.java
+++ services/cis/modules/java/contractinformationservice/src/main/java/gov/gsa/fas/contractservice/handler/ServiceHandler.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import gov.gsa.fas.contractservice.model.RequestWrapper;
 import gov.gsa.fas.contractservice.service.ContractService;
@@ -16,11 +18,24 @@ import gov.gsa.fas.contractservice.contract.PORecordsType;
 import gov.gsa.fas.contractservice.contract.PORequestType;
 
 public class ServiceHandler implements RequestHandler<RequestWrapper, RequestWrapper> {
+	
+	Logger logger = LoggerFactory.getLogger(ServiceHandler.class);
+	
 	public RequestWrapper handleRequest(RequestWrapper inputStream, Context context) {
 
+		logger.info("printing inputStream.getBody() : " + inputStream.getBody());
+		
+		if(inputStream.getBody()==null) {
+			logger.info("printing inputStream.getBody() is NULL ");
+		}
+		
 		//LambdaLogger logger = context.getLogger();
 		PORequestType reqns = ContractServiceUtil.unmarshall(inputStream.getBody(),
 				PORequestType.class);
+		
+		if(reqns==null) {
+			logger.info("issues with unmarshalling the RequestBody to Object  ");
+		}
 
 		List<PORecordsType> inPORecords = reqns.getPurchaseOrders();
 		GetContractDataResponse finalPORes = new GetContractDataResponse();
@@ -58,6 +73,7 @@ public class ServiceHandler implements RequestHandler<RequestWrapper, RequestWra
 			finalPORes.setCSDetails(csDetails);
 			inputStream.setBody(ContractServiceUtil.marshall(finalPORes));
 		} catch (Exception e) {
+			logger.error("Exception in ServiceHandler.handleRequest ", e);
 			e.printStackTrace();
 		}
 		return inputStream;
@@ -68,7 +84,9 @@ public class ServiceHandler implements RequestHandler<RequestWrapper, RequestWra
 		ServiceHandler s = new ServiceHandler();
 
 		RequestWrapper wrapper =new RequestWrapper();
-		wrapper.setBody(ContractConstants.TEST_MULTIPLE_PO_BODY);
+		wrapper.setBody("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:con=\"http://contract/\"><soapenv:Header/><soapenv:Body><con:PORequest><NumOfRecord>1</NumOfRecord><PurchaseOrders POLineNumber=\"01\"><PurchaseOrderNum>NMNJH753C8</PurchaseOrderNum><totalPOCost>12.75</totalPOCost> <ContractNum>47QSEA20T000E</ContractNum><BuyerCode></BuyerCode><RequisitionRecords requisitionLineNumber=\"1\"><requisitionNumber>POPlIT4200022</requisitionNumber><itemNumber>7510015904409</itemNumber>   <reportingOffice>N</reportingOffice><pricingZone>01</pricingZone>   </RequisitionRecords></PurchaseOrders></con:PORequest>  </soapenv:Body> </soapenv:Envelope>");
+				
+				//ContractConstants.TEST_MULTIPLE_PO_BODY);
 		RequestWrapper outputStream = s.handleRequest(wrapper, null);
 		System.out.println(outputStream.getBody());
 	}
