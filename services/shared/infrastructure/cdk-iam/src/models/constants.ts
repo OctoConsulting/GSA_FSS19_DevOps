f343c05145d245ddeb4141c3a0cddc41bb37068e
Balu Vyamajala <balu.vyamajala@initechglobal.com>
2021-03-09 18:12:31 -0500

    kms access

---
+++ services/shared/infrastructure/cdk-iam/src/models/constants.ts
@@ -0,0 +1,5 @@
+const KMS_ALIAS_DYNAMODB_ENCRYPT_AT_REST_CONTRACT = 'fss19/dynamodb/atrest/contract';
+const KMS_ALIAS_DYNAMODB_ENCRYPT_AT_REST_NSN = 'fss19/dynamodb/atrest/nsnrouting';
+
+const constants = { KMS_ALIAS_DYNAMODB_ENCRYPT_AT_REST_CONTRACT, KMS_ALIAS_DYNAMODB_ENCRYPT_AT_REST_NSN };
+export { constants };
