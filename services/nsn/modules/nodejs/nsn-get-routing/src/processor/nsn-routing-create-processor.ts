d363e0547329d6eaea816f2249fe3483082006d8
ruchir <ruchir.mehta@gsa.gov>
2021-03-17 09:55:37 -0400

    [P1][S3][NSN] - added validation for routing identifier code - POST API -
    [P1][S3][NSN] - added validation for routing identifier code - POST API -

--- services/nsn/modules/nodejs/nsn-get-routing/src/processor/nsn-routing-create-processor.ts
+++ services/nsn/modules/nodejs/nsn-get-routing/src/processor/nsn-routing-create-processor.ts
@@ -13,14 +13,29 @@ export const postNsn = async (event: APIGatewayProxyEvent, context: Context): Pr
     }
     console.log('1 body - ' + event.body);
 
-    const { routing_id, owa, is_civ_mgr, is_mil_mgr, ric, created_by } = JSON.parse(event.body);
-
+    let { routing_id, owa, is_civ_mgr, is_mil_mgr, ric, created_by } = JSON.parse(event.body);
+    
     if (!routing_id) {
-        return apiResponses._400({ message: 'Routing is mandetory to create NSN record' });
+        return apiResponses._400({ message: 'Routing is mandatory to create NSN record.' });
     }
+    routing_id = routing_id.trim(); // trimming routing id for extra spaces
     if (isNaN(routing_id.substring(0, 4))) {
-        return apiResponses._400({ message: 'Routing id should be numeric' });
+        return apiResponses._400({ message: 'Please check routing Id restrictions.' });
     }
+    if (routing_id.length<4 && routing_id.length!=2) {
+        return apiResponses._400({ message: 'Invalid routing Id, please check routing Id restrictions.' });
+    }
+    if (routing_id.length>15) {
+        return apiResponses._400({ message: 'Routing id can not be more than 15 characters.' });
+    }
+    // Setting valid values for Civ and Mil Manager
+    is_civ_mgr = (is_civ_mgr=="Y")?is_civ_mgr:"N";
+    is_mil_mgr = (is_mil_mgr=="Y")?is_mil_mgr:"N";
+
+    if((is_civ_mgr=="Y" || is_mil_mgr == "Y") && !ric) {
+        return apiResponses._400({ message: 'Routing identifier code is mandatory.' });
+    }
+
     let group_id = Number(routing_id.substring(0, 2));
 
     console.log('3 ' + routing_id);
@@ -31,7 +46,6 @@ export const postNsn = async (event: APIGatewayProxyEvent, context: Context): Pr
             routing_id: routing_id,
         },
     };
-    debugger;
     console.log('4 - params - ' + params);
     let existingNsnData = await dynamoDocumentClient.get(params).promise();
     console.log('5 existingNsnData - ' + existingNsnData);
