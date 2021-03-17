e37ef5f806a81be8321e705e006f60099cb15c8f
mahajay <mahajay@gmail.com>
2021-03-17 13:11:38 -0400

    [P1][S3] - Unit test coverage and additional validations on create NSN api.

--- services/nsn/modules/nodejs/nsn-get-routing/src/processor/nsn-routing-create-processor.ts
+++ services/nsn/modules/nodejs/nsn-get-routing/src/processor/nsn-routing-create-processor.ts
@@ -4,23 +4,38 @@ import { NsnData } from '../model/nsn-data';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { dynamoDocumentClient, getSettings } from '../config';
 import { apiResponses } from '../model/responseAPI';
-import { RSA_PSS_SALTLEN_MAX_SIGN } from 'constants';
+import { DynamoDB } from 'aws-sdk';
 
-export const postNsn = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
+export const postNsn = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     console.log('Saving the NSN data - ' + event);
     if (event.body === null) {
         return apiResponses._400({ message: 'No routing data provided to create NSN routing record.' });
     }
     console.log('1 body - ' + event.body);
 
-    const { routing_id, owa, is_civ_mgr, is_mil_mgr, ric, created_by } = JSON.parse(event.body);
+    let { routing_id, owa, is_civ_mgr, is_mil_mgr, ric, created_by } = JSON.parse(event.body);
 
     if (!routing_id) {
-        return apiResponses._400({ message: 'Routing is mandetory to create NSN record' });
+        return apiResponses._400({ message: 'Routing is mandatory to create NSN record.' });
     }
+    routing_id = routing_id.trim(); // trimming routing id for extra spaces
     if (isNaN(routing_id.substring(0, 4))) {
-        return apiResponses._400({ message: 'Routing id should be numeric' });
+        return apiResponses._400({ message: 'Please check routing Id restrictions.' });
     }
+    if (routing_id.length < 4 && routing_id.length != 2) {
+        return apiResponses._400({ message: 'Invalid routing Id, please check routing Id restrictions.' });
+    }
+    if (routing_id.length > 15) {
+        return apiResponses._400({ message: 'Routing id can not be more than 15 characters.' });
+    }
+    // Setting valid values for Civ and Mil Manager
+    is_civ_mgr = is_civ_mgr == 'Y' ? is_civ_mgr : 'N';
+    is_mil_mgr = is_mil_mgr == 'Y' ? is_mil_mgr : 'N';
+
+    if ((is_civ_mgr == 'Y' || is_mil_mgr == 'Y') && !ric) {
+        return apiResponses._400({ message: 'Routing identifier code is mandatory.' });
+    }
+
     let group_id = Number(routing_id.substring(0, 2));
 
     console.log('3 ' + routing_id);
@@ -31,9 +46,8 @@ export const postNsn = async (event: APIGatewayProxyEvent, context: Context): Pr
             routing_id: routing_id,
         },
     };
-    debugger;
     console.log('4 - params - ' + params);
-    let existingNsnData = await dynamoDocumentClient.get(params).promise();
+    let existingNsnData = await getDocumentDbClient().get(params).promise();
     console.log('5 existingNsnData - ' + existingNsnData);
     if (existingNsnData.Item != null) {
         return apiResponses._422({ message: 'NSN routing record already exists for the routing id - ' + routing_id });
@@ -56,11 +70,23 @@ export const postNsn = async (event: APIGatewayProxyEvent, context: Context): Pr
         console.log('8');
         const model = { TableName: getSettings().TABLE_NAME, Item: nsnData };
         console.log('9');
-        await dynamoDocumentClient.put(model).promise();
+        await getDocumentDbClient().put(model).promise();
         console.log('10 ' + model.Item);
         return apiResponses._201(model.Item);
     } catch (err) {
         console.log('Error ---- ' + err);
         return apiResponses._500({ message: 'Error creating NSN record' });
     }
+};
+
+const getDocumentDbClient = (): DynamoDB.DocumentClient => {
+    let options = {};
+
+    if (process.env.IS_OFFLINE) {
+        options = {
+            region: 'localhost',
+            endpoint: 'http://localhost:8000',
+        };
+    }
+    return new DynamoDB.DocumentClient(options);
 };
--- services/nsn/modules/nodejs/nsn-get-routing/test/processor/nsn-routing-post-processor.test.ts
+++ services/nsn/modules/nodejs/nsn-get-routing/test/processor/nsn-routing-post-processor.test.ts
@@ -1,46 +1,107 @@
-import { getNsn } from '../../src/processor/nsn-routing-get-processor';
+import { postNsn } from '../../src/processor/nsn-routing-create-processor';
+import { NsnData } from '../../src/model/nsn-data';
 import * as appConfig from '../../src/config';
 import * as nock from 'nock';
 import { expect } from 'chai';
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { mock, when, instance } from 'ts-mockito';
-import * as AWS from 'aws-sdk';
 import { DynamoDB } from 'aws-sdk';
-import * as AWSMock from 'aws-sdk-mock';
+import AWSMock from 'aws-sdk-mock';
+import AWS from 'aws-sdk';
 import * as LambdaTester from 'lambda-tester';
 import { GetItemInput } from 'aws-sdk/clients/dynamodb';
 
 describe('errorTests', () => {
     let mockEvent: APIGatewayProxyEvent = mock(<APIGatewayProxyEvent>{});
     const proxyEvent: APIGatewayProxyEvent = instance(mockEvent);
+    let mockNsnIntfc: NsnData = mock(<NsnData>{});
+    var mockNsnData: NsnData = mock(mockNsnIntfc);
 
-    it('PathParaneterErrors400', async () => {
-        when(mockEvent.pathParameters).thenReturn(null);
+    it('Null or empty routing ID for create API', async () => {
+        when(mockEvent.body).thenReturn(null);
 
-        let response = await getNsn(proxyEvent);
+        let response = await postNsn(proxyEvent);
         expect(response.statusCode).to.equal(400);
 
-        // invalid routing id parameter
-        when(mockEvent.pathParameters).thenReturn({ id: '1' });
-        response = await getNsn(proxyEvent);
-        expect(response.statusCode).to.equal(400);
-
-        // invalid routing id parameter value
-        when(mockEvent.pathParameters).thenReturn({ routingId: '1' });
-        response = await getNsn(proxyEvent);
-        expect(response.statusCode).to.equal(400);
-
-        // invalid routing id parameter value
-        when(mockEvent.pathParameters).thenReturn({ routingId: '123' });
-        response = await getNsn(proxyEvent);
-        expect(response.statusCode).to.equal(400);
-
-        // group id passed as string
-        when(mockEvent.pathParameters).thenReturn({ routingId: 'aabb' });
-        response = await getNsn(proxyEvent);
+        let body: string = '{}';
+        when(mockEvent.body).thenReturn(body);
         expect(response.statusCode).to.equal(400);
     });
 
+    it('Invalid routing ID(NaN) for create API', async () => {
+        when(mockEvent.body).thenReturn(null);
+
+        let body: string = '{"routing_id": "ab33"}';
+        when(mockEvent.body).thenReturn(body);
+        let response = await postNsn(proxyEvent);
+        expect(response.statusCode).to.equal(400);
+    });
+
+    it('Invalid routing ID(3 digit number) for create API', async () => {
+        when(mockEvent.body).thenReturn(null);
+
+        let body: string = '{"routing_id": "123"}';
+        when(mockEvent.body).thenReturn(body);
+        let response = await postNsn(proxyEvent);
+        expect(response.statusCode).to.equal(400);
+    });
+
+    it('Invalid routing ID(more than 15 digit number) for create API', async () => {
+        when(mockEvent.body).thenReturn(null);
+
+        let body: string = '{"routing_id": "1234234gjh323298934"}';
+        when(mockEvent.body).thenReturn(body);
+        let response = await postNsn(proxyEvent);
+        expect(response.statusCode).to.equal(400);
+    });
+
+    it('NSN record exists error for create API', async () => {
+        when(mockEvent.body).thenReturn(null);
+        let body: string = '{"routing_id": "1111"}';
+        when(mockEvent.body).thenReturn(body);
+        AWSMock.setSDKInstance(AWS);
+        AWSMock.mock(
+            'DynamoDB.DocumentClient',
+            'get',
+            (params: DynamoDB.DocumentClient.GetItemInput, callback: Function) => {
+                callback(null, { Item: {} });
+            }
+        );
+
+        let response = await postNsn(proxyEvent);
+        expect(response.statusCode).to.equal(422);
+        AWSMock.restore('DynamoDB.DocumentClient');
+    });
+
+    it('NSN record success for create API', async () => {
+        when(mockEvent.body).thenReturn(null);
+        let body: string = '{"routing_id": "1111"}';
+        when(mockEvent.body).thenReturn(body);
+        AWS.config.update({ region: 'us-east-1' });
+        AWSMock.setSDKInstance(AWS);
+        AWSMock.mock(
+            'DynamoDB.DocumentClient',
+            'get',
+            (params: DynamoDB.DocumentClient.GetItemInput, callback: Function) => {
+                callback(null, { Attributes: {} });
+            }
+        );
+
+        AWSMock.mock(
+            'DynamoDB.DocumentClient',
+            'put',
+            (params: DynamoDB.DocumentClient.PutItemInput, callback: Function) => {
+                callback(null, { Attributes: {} });
+            }
+        );
+
+        let response = await postNsn(proxyEvent);
+        expect(response.statusCode).to.equal(201);
+        AWSMock.restore('DynamoDB.DocumentClient');
+    });
+
+    /*
+    
     it('PathParaneterErrors404', async () => {
         AWSMock.setSDKInstance(AWS);
 
@@ -103,6 +164,7 @@ describe('errorTests', () => {
 
         AWSMock.restore('DynamoDB.DocumentClient');
     });
+    */
 });
 
 function mockDocumentClient() {
