1b52335f985a64bb807642bf818c73397017da98
Balu Vyamajala <balu.vyamajala@initechglobal.com>
2021-03-31 16:43:25 -0400

    lambda code.

--- services/contract-constant/infrastructure/cdk/src/models/constants.ts
+++ services/contract-constant/infrastructure/cdk/src/models/constants.ts
@@ -4,13 +4,13 @@ const LIVE_ALIAS_NAME = 'live';
 const MAX_PROVISIONED_CAPACITY_FOR_LAMBDA = 500;
 const LAMBDA_SUBNET_GROUP_NAME = 'Isolated';
 //TODO when actual artifact is deployed
-const NSN_ROUTING_LAMBDA_ZIP_PATH = 'services/contract-const/nsn-get-routing/index.zip';
+const LAMBDA_ZIP_PATH = 'services/contract-const/nsn-get-routing/index.zip';
 const constants = {
     API_PREFIX,
     LIVE_ALIAS_NAME,
     TABLE_PREFIX,
     MAX_PROVISIONED_CAPACITY_FOR_LAMBDA,
-    NSN_ROUTING_LAMBDA_ZIP_PATH,
+    LAMBDA_ZIP_PATH,
     LAMBDA_SUBNET_GROUP_NAME,
 };
 
