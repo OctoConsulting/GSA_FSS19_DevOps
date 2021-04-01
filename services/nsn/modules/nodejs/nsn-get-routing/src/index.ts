63fe06df5b531f0bf8c0056f644d6599aff97386
Balu Vyamajala <balu.vyamajala@initechglobal.com>
2021-04-01 18:35:14 -0400

    updated response.

--- services/nsn/modules/nodejs/nsn-get-routing/src/index.ts
+++ services/nsn/modules/nodejs/nsn-get-routing/src/index.ts
@@ -3,10 +3,15 @@ import { mainProcessor } from './processor/main-processor';
 
 export const handler = (event: APIGatewayProxyEvent, context: Context, callback: Callback) => {
     const mainProcess = async () => {
+        console.log('event', event);
         const response = await mainProcessor.start({
             body: event.body!,
         });
-        return response;
+        return {
+            statusCode: 202,
+            body: response,
+            isBase64Encoded: false,
+        };
     };
 
     mainProcess()
