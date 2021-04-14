b68f2a35ff8ba129a6cb5c35c8f2ce41d7e0abf5
mahajay <mahajay@gmail.com>
2021-04-14 19:21:04 -0400

    [P2][S1] - Creating customized pool for local connection and RDS proxy for cloud RDS connection

--- services/nsn/modules/nodejs/nsn-get-routing/src/config.ts
+++ services/nsn/modules/nodejs/nsn-get-routing/src/config.ts
@@ -1,5 +1,6 @@
 import { DynamoDB, RDS } from 'aws-sdk';
-import mysql2, { Connection, Pool } from 'mysql2';
+import mysql from 'mysql';
+import mysql2 from 'mysql2';
 
 let options = {};
 
@@ -12,6 +13,23 @@ if (process.env.IS_OFFLINE) {
 export const dynamoDocumentClient = new DynamoDB.DocumentClient(options);
 
 export const getDBSettings = () => {
+    let connectionConfig = {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+        ssl: 'Amazon RDS',
+        authPlugins: {
+            mysql_clear_password: () => () => {
+                new RDS.Signer().getAuthToken({
+                    region: process.env.AWS_REGION,
+                    hostname: process.env.DB_HOST,
+                    username: process.env.DB_NAME,
+                    port: 3306,
+                });
+            },
+        },
+    };
+
     return {
         TABLE_NAME:
             '`' +
@@ -21,6 +39,16 @@ export const getDBSettings = () => {
             (process.env.SHORT_ENV == undefined ? '' : '_' + process.env.SHORT_ENV) +
             '`',
         IS_OFFLINE: process.env.IS_OFFLINE,
+        // CONNECTION_POOL:
+        //     process.env.SHORT_ENV == 'local'
+        //         ? mysql2.createPool({
+        //               host: process.env.DB_HOST,
+        //               user: process.env.DB_USER,
+        //               password: process.env.DB_PWD,
+        //               database: process.env.DB_NAME,
+        //           })
+        //         : mysql2.createConnection(connectionConfig),
+
         CONNECTION_POOL:
             process.env.SHORT_ENV == 'local'
                 ? mysql2
@@ -31,19 +59,6 @@ export const getDBSettings = () => {
                           database: process.env.DB_NAME,
                       })
                       .promise()
-                : mysql2
-                      .createConnection({
-                          host: process.env.DB_HOST,
-                          port: 3306,
-                          user: process.env.DB_USER,
-                          password: new RDS.Signer().getAuthToken({
-                              region: process.env.AWS_REGION,
-                              hostname: process.env.DB_HOST,
-                              username: process.env.DB_NAME,
-                              port: 3306,
-                          }),
-                          database: process.env.DB_NAME,
-                      })
-                      .promise(),
+                : mysql.createConnection(connectionConfig),
     };
 };
