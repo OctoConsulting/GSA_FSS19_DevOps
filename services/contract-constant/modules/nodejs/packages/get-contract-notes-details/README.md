2d7c7b1024c7b8301d31901e265180ab4bed735b
ruchir <ruchir.mehta@gsa.gov>
2021-04-19 18:50:17 -0400

    [P2][S1][Contract-constant-notes] Added Handler for NotesDetails

---
+++ services/contract-constant/modules/nodejs/packages/get-contract-notes-details/README.md
@@ -0,0 +1,23 @@
+## `About`
+
+    This function retuns nsn routing record for a give routing no
+
+## Run locally
+
+```
+npm install -g lambda-local
+
+execute the following commands before deploy
+npm install serverless-dynamodb-local
+serverless dynamodb install
+
+sls offline start --location .
+
+```
+
+## Build
+
+```
+With Development Profile: npm run build-dev
+With Prod Profile: npm run build-prod
+```
---
+++ services/contract-constant/modules/nodejs/packages/get-contract-notes-details/src/processor/get-notes-details.ts
@@ -0,0 +1,85 @@
+'use strict';
+import { DynamoDB } from 'aws-sdk';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { apiResponses, response } from '../model/responseAPI';
+import { getSettings } from '../config';
+import { NotesDetailsData } from '../model/notes-details-data'
+
+export const getNotesDetailsData = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    if (!event.body) {
+        return apiResponses._400({ message: 'INVALID Request : NO request provided' });
+    }
+
+    let { d430_rpt_off ,d430_note_cd, d430_rec_type } = JSON.parse(event.body);
+
+
+    if ( !d430_rpt_off || !d430_rec_type) {
+        console.log(' d430_rpt_off : ' + d430_rpt_off + ' | d430_rec_type : ' + d430_rec_type )
+        return apiResponses._400({
+            message: 'INVALID_REQUEST : Missing required Parameter in Request.',
+        });
+    }
+
+    let notesData ;
+    try {
+        // let params = {
+        //     TableName: getSettings().TABLE_NAME,
+        //     Key: {
+        //         pk: d430_rpt_off,
+        //         sk: "detail_d430_"+d430_rec_type.toUpperCase()+"_"+d430_note_cd,
+        //     },
+        // };
+
+        const params = {
+            TableName: getSettings().TABLE_NAME,
+            KeyConditionExpression: '#pk = :pk and begins_with(#sk, :sk)',
+            ExpressionAttributeNames:{
+              "#pk": "pk",
+              "#sk": 'sk'
+            },
+            ExpressionAttributeValues: {
+                ":pk": d430_rpt_off,
+                ":sk": 'detail_d430_'+d430_rec_type+'_'+(d430_note_cd?d430_note_cd:'')
+            }
+          }
+
+        notesData = await getDocumentDbClient().query(params).promise();
+    
+      //  let notesData = await getDocumentDbClient().get(params).promise();
+        console.log("Notes details : " + JSON.stringify(notesData))
+
+        if (!notesData.Items || notesData.Items.length == 0) {
+            return apiResponses._404({ message: 'No Notes Details for d430_rpt_off : ' + d430_rpt_off +  ' | d430_note_cd : ' + d430_note_cd +  ' | d430_rec_type : ' + d430_rec_type  });
+        }
+
+        notesData.Items.forEach(element => {
+            var n = element.sk.lastIndexOf('_');
+            var result = element.sk.substring(n + 1);
+            element.note_cd = result;
+        });
+
+        const notesDetailsData: NotesDetailsData = notesData.Items;
+        return apiResponses._200(notesDetailsData);
+
+
+    } catch (err) {
+        console.log('Error >>>>>> ' + err);
+        return apiResponses._500({ message: 'Error fetching record for d430_rpt_off : ' + d430_rpt_off + ' | d430_note_cd : ' + d430_note_cd + ' | d430_rec_type : ' + d430_rec_type   });
+    }
+};
+
+const getDocumentDbClient = (): DynamoDB.DocumentClient => {
+    let options = {};
+
+    if (process.env.IS_OFFLINE) {
+        options = {
+            region: 'localhost',
+            endpoint: 'http://localhost:8000',
+        };
+    }
+    return new DynamoDB.DocumentClient(options);
+};
+
+module.exports = {
+    getNotesDetailsData: getNotesDetailsData,
+};
