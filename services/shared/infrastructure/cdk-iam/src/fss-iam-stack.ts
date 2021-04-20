efd1d79dd48055c4f454ddc8427edc22438df87f
Balu Vyamajala <balu.vyamajala@initechglobal.com>
2021-04-20 15:36:11 -0400

    initial code to create saml provider and user.

--- services/shared/infrastructure/cdk-iam/src/fss-iam-stack.ts
+++ services/shared/infrastructure/cdk-iam/src/fss-iam-stack.ts
@@ -2,12 +2,19 @@ import * as cdk from '@aws-cdk/core';
 import { EnvHelper } from './helper/env-helper';
 import { EnvParameters } from './models/env-parms';
 import { PolicyConstruct } from './constructs/policy-construct';
+import { IdentityProviderConstruct } from './constructs/identity-provider-construct';
 export class FssIamStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         const stackContext = this.node.tryGetContext(`${id}-${process.env.SHORT_ENV}`);
         const envParameters: EnvParameters = new EnvHelper().getEnvironmentParams(stackContext);
+
+        new IdentityProviderConstruct(this, 'okta', {
+            samlMetadataFilePath: envParameters.identityProvider.metadataPath,
+            userName: envParameters.identityProvider.userName,
+        });
+
         envParameters.iamSets.forEach((iamSet) => {
             new PolicyConstruct(this, `${iamSet.groupName}`, {
                 iamSet,
---
+++ services/shared/infrastructure/cdk-iam/src/models/identity-provider.ts
@@ -0,0 +1,4 @@
+export interface IdentityProvider {
+    metadataPath: string;
+    userName: string;
+}
