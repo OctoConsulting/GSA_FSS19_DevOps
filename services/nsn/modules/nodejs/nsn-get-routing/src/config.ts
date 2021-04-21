24b515ac3e83956438c3a438ce0f0be9e0e2a3e8
mahajay <mahajay@gmail.com>
2021-04-21 10:37:17 -0400

    [P2][S1] - Refactoring of the code to remove CONNECTION attribute from config and using executeQuery function for all the apis.

--- services/nsn/modules/nodejs/nsn-get-routing/src/config.ts
+++ services/nsn/modules/nodejs/nsn-get-routing/src/config.ts
@@ -32,25 +32,6 @@ export const getDBSettings = () => {
             (process.env.SHORT_ENV == undefined ? '' : '_' + process.env.SHORT_ENV) +
             '`',
         IS_OFFLINE: process.env.IS_OFFLINE,
-        CONNECTION:
-            process.env.SHORT_ENV == 'local'
-                ? mysql2.createConnection({
-                      host: process.env.DB_HOST,
-                      user: process.env.DB_USER,
-                      password: process.env.DB_PWD,
-                      database: process.env.DB_NAME,
-                  })
-                : mysql2.createConnection({
-                      host: process.env.DB_HOST,
-                      port: 3306,
-                      user: process.env.DB_USER,
-                      ssl: { rejectUnauthorized: false },
-                      password: token,
-                      database: process.env.DB_NAME,
-                      authPlugins: {
-                          mysql_clear_password: () => () => token,
-                      },
-                  }),
     };
 };
 
