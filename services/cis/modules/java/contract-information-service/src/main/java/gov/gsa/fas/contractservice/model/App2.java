3eb5f398a9c69d5edd5c8b2a8ba59c3234947f3e
ruchir <ruchir.mehta@gsa.gov>
2021-04-29 10:05:26 -0400

    fixed the compilation issues.

--- services/cis/modules/java/contract-information-service/src/main/java/gov/gsa/fas/contractservice/model/App2.java
+++ services/cis/modules/java/contract-information-service/src/main/java/gov/gsa/fas/contractservice/model/App2.java
@@ -1,7 +1,20 @@
 package gov.gsa.fas.contractservice.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
 
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
@@ -17,6 +30,7 @@ import com.amazonaws.services.dynamodbv2.model.KeyType;
 import com.amazonaws.services.dynamodbv2.model.Projection;
 import com.amazonaws.services.dynamodbv2.model.ProjectionType;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.google.gson.Gson;
 
 
 public class App2 {
