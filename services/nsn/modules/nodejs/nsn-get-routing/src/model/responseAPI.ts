2ecb1d6b79453c9e2abfd00ad5ffc76f29b83fd1
ruchir <ruchir.mehta@gsa.gov>
2021-05-03 18:06:50 -0400

    removed headers from NSN get routing

--- services/nsn/modules/nodejs/nsn-get-routing/src/model/responseAPI.ts
+++ services/nsn/modules/nodejs/nsn-get-routing/src/model/responseAPI.ts
@@ -8,77 +8,42 @@ export const response = function (statusCode: number, body: { [key: string]: any
 export const apiResponses = {
     _200: (body: { [key: string]: any }) => {
         return {
-            headers: {
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Origin": "https://fss-ui-dev.fss19-dev.fcs.gsa.gov/",
-                "Access-Control-Allow-Methods": "OPTIONS,POST,PUT,GET"
-            },
             statusCode: 200,
             body: JSON.stringify(body, null, 2),
         };
     },
     _201: (body: { [key: string]: any }) => {
         return {
-            headers: {
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Origin": "https://fss-ui-dev.fss19-dev.fcs.gsa.gov/",
-                "Access-Control-Allow-Methods": "OPTIONS,POST,PUT,GET"
-            },
             statusCode: 201,
             body: JSON.stringify(body, null, 2),
         };
     },
     _204: (body: { [key: string]: any }) => {
         return {
-            headers: {
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Origin": "https://fss-ui-dev.fss19-dev.fcs.gsa.gov/",
-                "Access-Control-Allow-Methods": "OPTIONS,POST,PUT,GET"
-            },
             statusCode: 204,
             body: JSON.stringify(body, null, 2),
         };
     },
     _400: (body: { [key: string]: any }) => {
         return {
-            headers: {
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Origin": "https://fss-ui-dev.fss19-dev.fcs.gsa.gov/",
-                "Access-Control-Allow-Methods": "OPTIONS,POST,PUT,GET"
-            },
             statusCode: 400,
             body: JSON.stringify(body, null, 2),
         };
     },
     _404: (body: { [key: string]: any }) => {
         return {
-            headers: {
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Origin": "https://fss-ui-dev.fss19-dev.fcs.gsa.gov/",
-                "Access-Control-Allow-Methods": "OPTIONS,POST,PUT,GET"
-            },
             statusCode: 404,
             body: JSON.stringify(body, null, 2),
         };
     },
     _422: (body: { [key: string]: any }) => {
         return {
-            headers: {
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Origin": "https://fss-ui-dev.fss19-dev.fcs.gsa.gov/",
-                "Access-Control-Allow-Methods": "OPTIONS,POST,PUT,GET"
-            },
             statusCode: 422,
             body: JSON.stringify(body, null, 2),
         };
     },
     _500: (body: { [key: string]: any }) => {
         return {
-            headers: {
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Origin": "https://fss-ui-dev.fss19-dev.fcs.gsa.gov/",
-                "Access-Control-Allow-Methods": "OPTIONS,POST,PUT,GET"
-            },
             statusCode: 500,
             body: JSON.stringify(body, null, 2),
         };
